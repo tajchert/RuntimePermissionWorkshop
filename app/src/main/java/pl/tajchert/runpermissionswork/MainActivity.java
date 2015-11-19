368979ef715683bf20cfbc084a40b03553572dbd
Michal Tajchert <mtajchert@gmail.com>
2015-11-19 23:09:52 +0100

    Task one, handle lack of permission.

--- app/src/main/java/pl/tajchert/runpermissionswork/MainActivity.java
+++ app/src/main/java/pl/tajchert/runpermissionswork/MainActivity.java
@@ -1,9 +1,13 @@
 package pl.tajchert.runpermissionswork;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import butterknife.Bind;
@@ -37,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
     private void callTest() {
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + "11122233"));
-        startActivity(intent);
+        startActivity(intent);//TODO Handle lack of permission
     }
 }
