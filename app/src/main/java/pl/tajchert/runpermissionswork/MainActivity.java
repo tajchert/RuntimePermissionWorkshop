e092e281b34445a84af3eb19531f6c875acf1ee6
Michal Tajchert <mtajchert@gmail.com>
2015-11-19 23:36:19 +0100

    Task two solved, requesting a permission and handling its result.

--- app/src/main/java/pl/tajchert/runpermissionswork/MainActivity.java
+++ app/src/main/java/pl/tajchert/runpermissionswork/MainActivity.java
@@ -16,6 +16,7 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_CALL = 32;
 
     @Bind(R.id.main_layout)
     View mLayout;//We will use it later
@@ -34,18 +35,33 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.buttonCall)
     public void clickButtCall() {
-        callTest();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "We don't have any permission for calling");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_CALL);
+        } else {
+            callTest();
+        }
     }
-
 
     private void callTest() {
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + "11122233"));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            Log.e(TAG, "We don't have any permission for calling");
-            //TODO request a permission using ActivityCompat.requestPermissions(), handle result in onRequestPermissionsResult()
-            return;
+        try {
+            startActivity(intent);
+        } catch (SecurityException e) {
+            e.printStackTrace();
         }
-        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case (REQUEST_CODE_CALL):
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callTest();
+                }
+                break;
+            //Here goes other permissions, for example if you ask for Location permission, here handle as well result
+        }
     }
 }
